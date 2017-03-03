a478e7edaa9d959984e50d57d10a0b69cd2275bd
nour <plasmazxzx@gmail.com>
2017-03-04 01:34:29 +0200

    Test

---
+++ app/src/main/java/com/example/nour/makssab/Activity/Home.java
@@ -0,0 +1,26 @@
+package com.example.nour.makssab.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.example.nour.makssab.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+}
