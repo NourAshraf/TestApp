9ca5c80a635bed1f80c552121406a28b29ab459d
nour <plasmazxzx@gmail.com>
2017-08-26 17:55:10 +0200

    Test

--- app/src/main/java/com/example/nour/makssab/Activity/Members.java
+++ app/src/main/java/com/example/nour/makssab/Activity/Members.java
@@ -1,92 +1,3 @@
-<<<<<<< HEAD
-package com.example.nour.makssab.Activity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import com.example.nour.makssab.Adapter.MembersAdapter;
-import com.example.nour.makssab.Model.MembersModel;
-import com.example.nour.makssab.R;
-
-import java.util.ArrayList;
-
-public class Members extends AppCompatActivity {
-    private ListView lvMembersList;
-    private ArrayList<MembersModel> members;
-    private ImageView mImageViewBack;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_members);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
-        setSupportActionBar(toolbar);
-        lvMembersList = (ListView) findViewById(R.id.lvMembers);
-        members = new ArrayList<MembersModel>();
-        members.add(new MembersModel("طارق الفهد","110 متابع",""));
-        members.add(new MembersModel("طارق الفهد","110 متابع",""));
-        members.add(new MembersModel("طارق الفهد","110 متابع",""));
-        members.add(new MembersModel("طارق الفهد","110 متابع",""));
-        lvMembersList.setAdapter(new MembersAdapter(getApplicationContext(),members));
-        onVariables();
-
-    }
-    private void onVariables() {
-
-        mImageViewBack = (ImageView) findViewById(R.id.ivBack);
-        mImageViewBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Home.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-        @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.home_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_search:
-                Intent mIntentSearch=new Intent(getApplicationContext(),Search.class);
-                startActivity(mIntentSearch);
-                break;
-
-            case R.id.action_Adv_Favorites:
-                Intent mIntentAdvFavorites=new Intent(getApplicationContext(),MemberFavorites.class);
-                startActivity(mIntentAdvFavorites);
-                break;
-
-            case R.id.action_New_Account:
-                Intent mIntentNewAccount=new Intent(getApplicationContext(),NewAccount.class);
-                startActivity(mIntentNewAccount);
-                break;
-
-            case R.id.action_Login:
-                Intent mIntentLogin=new Intent(getApplicationContext(),Login.class);
-                startActivity(mIntentLogin);
-                break;
-        }
-        return true;
-    }
-
-}
-=======
 package com.example.nour.makssab.Activity;
 
 import android.content.Intent;
@@ -104,6 +15,7 @@ import android.widget.TextView;
 import com.example.nour.makssab.Adapter.MembersAdapter;
 import com.example.nour.makssab.Model.MembersModel;
 import com.example.nour.makssab.R;
+
 
 import java.util.ArrayList;
 
@@ -152,6 +64,5 @@ public class Members extends AppCompatActivity {
             }
         });
     }
-
 }
->>>>>>> orgin/master
+
--- app/src/main/java/com/example/nour/makssab/Adapter/MembersAdapter.java
+++ app/src/main/java/com/example/nour/makssab/Adapter/MembersAdapter.java
@@ -1,4 +1,65 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
+//package com.example.nour.makssab.Adapter;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import com.example.nour.makssab.Model.MembersModel;
+//import com.example.nour.makssab.R;
+//
+//import java.util.ArrayList;
+//
+///**
+// * Created by Kareem on 3/6/2017.
+// */
+//
+//public class MembersAdapter extends BaseAdapter {
+//    Context context;
+//    ArrayList<MembersModel> members=new ArrayList<MembersModel>();
+//
+//    public MembersAdapter(Context context, ArrayList<MembersModel> members) {
+//        this.context = context;
+//        this.members = members;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return members.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return null;
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        LayoutInflater layoutInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View view= layoutInflater.inflate(R.layout.members,parent,false);
+//        TextView Name=(TextView)view.findViewById(R.id.tvMembersName);
+//        TextView Follow=(TextView)view.findViewById(R.id.tvMembersFollow);
+//        TextView TextProfile=(TextView) view.findViewById(R.id.bMembersProfile);
+//
+//        MembersModel membersModel=members.get(position);
+//
+//        Name.setText(membersModel.getName());
+//        Follow.setText(membersModel.getFollow());
+//        TextProfile.setText(membersModel.getImageProfile());
+//
+//        return view;
+//    }
+//}
+//=======
 package com.example.nour.makssab.Adapter;
 
 import android.content.Context;
@@ -53,69 +114,7 @@ public class MembersAdapter extends BaseAdapter {
         MembersModel membersModel=members.get(position);
 
         Name.setText(membersModel.getName());
-        Follow.setText(membersModel.getFollow());
-        TextProfile.setText(membersModel.getImageProfile());
 
         return view;
     }
 }
-=======
-package com.example.nour.makssab.Adapter;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.example.nour.makssab.Model.MembersModel;
-import com.example.nour.makssab.R;
-
-import java.util.ArrayList;
-
-/**
- * Created by Kareem on 3/6/2017.
- */
-
-public class MembersAdapter extends BaseAdapter {
-    Context context;
-    ArrayList<MembersModel> members=new ArrayList<MembersModel>();
-
-    public MembersAdapter(Context context, ArrayList<MembersModel> members) {
-        this.context = context;
-        this.members = members;
-    }
-
-    @Override
-    public int getCount() {
-        return members.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater layoutInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view= layoutInflater.inflate(R.layout.members,parent,false);
-        TextView Name=(TextView)view.findViewById(R.id.tvMembersName);
-        TextView Follow=(TextView)view.findViewById(R.id.tvMembersFollow);
-        TextView TextProfile=(TextView) view.findViewById(R.id.bMembersProfile);
-
-        MembersModel membersModel=members.get(position);
-
-        Name.setText(membersModel.getName());
-
-        return view;
-    }
-}
->>>>>>> orgin/master
